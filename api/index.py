09a3be2224aa2622284667f6e1420b10be0d3d94
MSCMDD <MSCAoMe@outlook.com>
2025-02-14 17:15:34 +0800

    fix: 解决跨域请求问题

--- api/index.py
+++ api/index.py
@@ -1,27 +1,36 @@
 from api.MineStatus import MineStatus
 from fastapi import FastAPI, Request 
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses  import JSONResponse
 import uvicorn
 
-app = FastAPI()
+api = FastAPI()
 
-@app.get("/")
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@api.get("/")
 async def Status(request: Request):
     host = request.query_params.get('ip')
     response = await MineStatus.status(host)
     return response
 
-@app.get("/java/")
+@api.get("/java/")
 async def Status(request: Request):
     host = request.query_params.get('ip')
     response = await MineStatus.java_status(host)
     return response
 
-@app.get("/bedrock/")
+@api.get("/bedrock/")
 async def Status(request: Request):
     host = request.query_params.get('ip')
     response = await MineStatus.bedrock_status(host)
     return response
 
 if __name__ == "__main__":
-    uvicorn.run(app,  host="0.0.0.0", port=3000)
+    uvicorn.run(api,  host="0.0.0.0", port=3000)
