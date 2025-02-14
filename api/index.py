bb479d626bd28b58ecb8c5f459468347bdfa3389
wling-art <wlingzhenyu@163.com>
2025-02-15 00:31:56 +0800

    fix: 无效的任务创建

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-
 from MineStatus import handle_bedrock, handle_java, status
 
 app = FastAPI()
